d0ef84853befbeff75bd11e848b30dfacc2224de
Jed Wesley-Smith <jed@atlassian.com>
2015-05-21 14:31:02 +1000

    repackaged into separate compilation units

--- src/main/scala/hwsl2/Hash.scala
+++ src/main/scala/hwsl2/Hash.scala
@@ -1,46 +1,5 @@
 package hwsl2
 
-class MutableHash private (m: SL2) extends Mutable {
-
-  def this() = this(new SL2())
-
-  def copy() = new MutableHash(m.copy())
-
-  def canEqual(a: Any) = a.isInstanceOf[MutableHash]
-
-  override def equals(that: Any): Boolean = that match {
-    case that: MutableHash => that.canEqual(this) && this.hashCode == that.hashCode
-    case _ => false
-  }
-
-  override def hashCode: Int = 31 + m.hashCode
-
-  def withCopy_(f: MutableHash => Unit): MutableHash = {
-    val c = copy(); f(c); c
-  }
-
-  def append(buf: Array[Byte]) {
-    m.mulBufR(buf)
-  }
-
-  def prepend(buf: Array[Byte]) {
-    m.mulBufL(buf)
-  }
-
-  def foldAppend(bufs: Seq[Array[Byte]]) {
-    bufs.foldLeft(()) { (_, buf) =>
-      append(buf)
-    }
-  }
-
-  def foldPrepend(bufs: Seq[Array[Byte]]) {
-    bufs.foldRight(()) { (buf, _) =>
-      prepend(buf)
-    }
-  }
-
-}
-
 class Hash private (mh: MutableHash) extends Immutable {
 
   def this() = this(new MutableHash())
@@ -69,9 +28,7 @@ class Hash private (mh: MutableHash) extends Immutable {
 }
 
 object Hash {
-
   def unit = new Hash()
 
   def hash(buf: Array[Byte]) = new Hash().append(buf)
-
 }
---
+++ src/main/scala/hwsl2/MutableHash.scala
@@ -0,0 +1,33 @@
+package hwsl2
+
+class MutableHash private (m: SL2) extends Mutable {
+
+  def this() = this(new SL2())
+
+  def copy() = new MutableHash(m.copy())
+
+  def canEqual(a: Any) = a.isInstanceOf[MutableHash]
+
+  override def equals(that: Any): Boolean = that match {
+    case that: MutableHash => that.canEqual(this) && this.hashCode == that.hashCode
+    case _ => false
+  }
+
+  override def hashCode: Int = 31 + m.hashCode
+
+  def withCopy_(f: MutableHash => Unit): MutableHash = {
+    val c = copy(); f(c); c
+  }
+
+  def append(buf: Array[Byte]): Unit =
+    m.mulBufR(buf)
+
+  def prepend(buf: Array[Byte]): Unit =
+    m.mulBufL(buf)
+
+  def foldAppend(bufs: Seq[Array[Byte]]): Unit =
+    bufs.foreach { append(buf) }
+
+  def foldPrepend(bufs: Seq[Array[Byte]]): Unit =
+    bufs.foldRight(()) { (buf, _) => prepend(buf) } 
+}
